77b564482c89503d7d9e9d40e990e358da61a9bc
ishaa-p7 <ishapatel1207@gmail.com>
2026-01-30 11:46:50 +0530

    connection established

--- jdbc/JDBC/src/DemoJdbc.java
+++ jdbc/JDBC/src/DemoJdbc.java
@@ -6,10 +6,14 @@ public class DemoJdbc {
         String url="jdbc:postgresql://localhost:5432/demo";
         String uname="postgres";
         String pass="postgres123";
-
+        String sql = "select sname from student where sid=1";
         Class.forName("org.postgresql.Driver");
 Connection con= DriverManager.getConnection(url,uname,pass);
-
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        rs.next();
+        con.close();
+        System.out.println("Connection closed");
         System.out.println("ce");
     }
 }
