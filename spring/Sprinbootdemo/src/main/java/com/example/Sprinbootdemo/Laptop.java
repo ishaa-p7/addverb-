9cf6c2b5a0483db4a1082a1e2938f8613c6ff807
ishaa-p7 <ishapatel1207@gmail.com>
2026-02-02 13:09:44 +0530

    SOME MORE CONCEPTS

---
+++ spring/Sprinbootdemo/src/main/java/com/example/Sprinbootdemo/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.Sprinbootdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile(){
+        System.out.println("compiling");
+    }
+
+}
--- spring/Sprinbootdemo/src/main/java/com/example/Sprinbootdemo/SprinbootdemoApplication.java
+++ spring/Sprinbootdemo/src/main/java/com/example/Sprinbootdemo/SprinbootdemoApplication.java
@@ -2,13 +2,23 @@ package com.example.Sprinbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SprinbootdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprinbootdemoApplication.class, args);
-		System.out.println("HELLO WORLD");
+		ApplicationContext context=SpringApplication.run(SprinbootdemoApplication.class, args);
+
+
+		Alien obj=context.getBean(Alien.class);
+		obj.code();
+
+		Alien obj1=context.getBean(Alien.class);
+		obj1.code();
+
+		Laptop lap=context.getBean(Laptop.class);
+		lap.compile();
 
 	}
 
